a9f6123e35bdd5af6c416c3a99abcbf35421ca53
Chris Fraser <cwf@aya.yale.edu>
1995-07-25 20:34:24 +0000

    Initial revision

---
+++ lib/assert.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static char rcsid[] = "$Id$";
+
+int _assert(char *e, char *file, int line) {
+	fprintf(stderr, "assertion failed:");
+	if (e)
+		fprintf(stderr, " %s", e);
+	if (file)
+		fprintf(stderr, " file %s", file);
+	fprintf(stderr, " line %d\n", line);
+	fflush(stderr);
+	abort();
+	return 0;
+}
