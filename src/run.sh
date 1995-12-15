ad05158895b4ee050abaa5b2c4a71139a89b21eb
David Hanson <drh@drhanson.net>
1995-12-15 22:58:26 +0000

    Echoes remote host name; added RCS Id keyword.

--- src/run.sh
+++ src/run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+# $Id$
 # ../run target-os test [ remotehost ]
 
 # set -x
 case "$1" in
-symbolic-*)	target=symbolic; include=mips/`expr "$1" : '.*-\(.*\)'` ;;
+symbolic-irix)	target=$1; include=mips/irix ;;
+symbolic-osf)	target=$1; include=alpha/osf ;;
 *)		target=$1;
 		include=`expr "$1" : '\(.*\)-'`/`expr "$1" : '.*-\(.*\)'` ;;
 esac
@@ -17,7 +19,7 @@ fi
 
 BUILDDIR=${BUILDDIR-../}
 echo ${BUILDDIR}rcc -target=$target $2: 1>&2
-$LCC -N -S -B${BUILDDIR} -I../../../include/$include -Usun -Uvax -Umips -Ux86 \
+$LCC -N -S -B${BUILDDIR} -I../../../include/$include -Ualpha -Usun -Uvax -Umips -Ux86 \
 	 -Wf-errout=$2.2 -D`expr "$1" : '\(.*\)-'` -Wf-g0 -Wf-target=$target ../../../tst/$2.c
 if [ $? != 0 ]; then remotehost=noexecute; fi
 sed 's|\.\./\.\./\.\./tst/||g' <$2.2 >$2.tmp; mv $2.tmp $2.2
@@ -31,12 +33,14 @@ fi
 
 case "$remotehost" in
 noexecute)	exit 0 ;;
-""|"-")	$LCC -o $2 $2.s -lm; ./$2 <../../../tst/$2.0 >$2.1 ;;
-*)	rcp $2.s $remotehost:
+""|"-")	echo "   executing" $2: 1>&2
+	$LCC -o $2 $2.s -lm; ./$2 <../../../tst/$2.0 >$2.1 ;;
+*)	echo "   executing" $2 on $remotehost: 1>&2
+	rcp $2.s $remotehost:
 	if expr "$remotehost" : '.*@' >/dev/null ; then
 		remotehost="`expr $remotehost : '.*@\(.*\)'` -l `expr $remotehost : '\(.*\)@'`"
 	fi
-	rsh $remotehost "$LCC -o $2 $2.s -lm;./$2;rm $2 $2.[so]" <../../../tst/$2.0 >$2.1
+	rsh $remotehost "cc -o $2 $2.s -lm;./$2;rm -f $2 $2.[so]" <../../../tst/$2.0 >$2.1
 	;;
 esac
 if [ ! -r $2.1bk ]; then
