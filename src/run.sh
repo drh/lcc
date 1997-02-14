171a589d2410bbb292c34ccf4f68cfd7a76244b9
David Hanson <drh@drhanson.net>
1997-02-14 18:22:04 +0000

    Merge changes for 3.6; cut extraneous symbolic dependencies; add liblcc.a.

--- src/run.sh
+++ src/run.sh
@@ -1,52 +1,53 @@
 #!/bin/sh
 # $Id$
-# ../run target-os test [ remotehost ]
+# run .../target/os/tst/foo.s [ remotehost ]
 
 # set -x
-case "$1" in
-symbolic-irix)	target=$1; include=mips/irix ;;
-symbolic-osf)	target=$1; include=alpha/osf ;;
-*)		target=$1;
-		include=`expr "$1" : '\(.*\)-'`/`expr "$1" : '.*-\(.*\)'` ;;
-esac
-remotehost=${3-$REMOTEHOST} LCC=${LCC-lcc}
+target=`echo $1 | awk -F/ '{ print $(NF-3) }'`
+os=`echo $1 | awk -F/ '{ print $(NF-2) }'`
+dir=$target/$os; idir=include/$dir
+remotehost=${2-$REMOTEHOST}
 
-cd tst
-if [ ! -r ../../../include/$include ]; then
-	echo 2>&1 $0: unknown combination '"'$target'"'
+case "$1" in
+*symbolic*)	remotehost=noexecute ;;
+esac
+
+if [ ! -d "$target/$os" -o ! -d "$idir" ]; then
+	echo 2>&1 $0: unknown combination '"'$target/$os'"'
 	exit 1
 fi
 
-BUILDDIR=${BUILDDIR-../}
-echo ${BUILDDIR}rcc -target=$target $2: 1>&2
-$LCC -N -S -B${BUILDDIR} -I../../../include/$include -Ualpha -Usun -Uvax -Umips -Ux86 \
-	 -Wf-errout=$2.2 -D`expr "$1" : '\(.*\)-'` -Wf-g0 -Wf-target=$target ../../../tst/$2.c
+C=`basename $1 .s`
+BUILDDIR=${BUILDDIR-./} LCC="${LCC-${BUILDDIR}/lcc} -Wo-lccdir=$BUILDDIR"
+TSTDIR=${TSTDIR-${BUILDDIR}/$dir/tst}
+if [ ! -d $TSTDIR ]; then mkdir -p $TSTDIR; fi
+
+echo ${BUILDDIR}/rcc -target=$target/$os $1: 1>&2
+$LCC -N -S -B${BUILDDIR}/ -I$idir -Ualpha -Usun -Uvax -Umips -Ux86 \
+	-Wf-errout=$TSTDIR/$C.2 -D$target -Wf-g0 \
+	-Wf-target=$target-$os -o $1 tst/$C.c
 if [ $? != 0 ]; then remotehost=noexecute; fi
-sed 's|\.\./\.\./\.\./tst/||g' <$2.2 >$2.tmp; mv $2.tmp $2.2
-diff ../../../tst/$2.2 $2.2
-if [ ! -r $2.sbk ]; then
-	mv $2.s $2.sbk
-	cp $2.sbk $2.s
-else
-	if diff $2.sbk $2.s; then exit 0; fi
+if [ -r $dir/tst/$C.2bk ]; then
+	diff $dir/tst/$C.2bk $TSTDIR/$C.2
+fi
+if [ -r $dir/tst/$C.sbk ]; then
+	if diff $dir/tst/$C.sbk $TSTDIR/$C.s; then exit 0; fi
 fi
 
 case "$remotehost" in
 noexecute)	exit 0 ;;
-""|"-")	echo "   executing" $2: 1>&2
-	$LCC -o $2 $2.s -lm; ./$2 <../../../tst/$2.0 >$2.1 ;;
-*)	echo "   executing" $2 on $remotehost: 1>&2
-	rcp $2.s $remotehost:
+""|"-")	echo "   executing" $C: 1>&2
+	$LCC -o $TSTDIR/$C $1 ${BUILDDIR}/liblcc.a -lm; $TSTDIR/$C <tst/$C.0 >$TSTDIR/$C.1 ;;
+*)	echo "   executing" $C on $remotehost: 1>&2
+	rcp $1 $remotehost:
 	if expr "$remotehost" : '.*@' >/dev/null ; then
 		remotehost="`expr $remotehost : '.*@\(.*\)'` -l `expr $remotehost : '\(.*\)@'`"
 	fi
-	rsh $remotehost "cc -o $2 $2.s -lm;./$2;rm -f $2 $2.[so]" <../../../tst/$2.0 >$2.1
+	rsh $remotehost "cc -o $C $C.s -lm;./$C;rm -f $C $C.[so]" <tst/$C.0 >$TSTDIR/$C.1
 	;;
 esac
-if [ ! -r $2.1bk ]; then
-	mv $2.1 $2.1bk
-	cp $2.1bk $2.1
-	exit 0
+if [ -r $dir/tst/$C.1bk ]; then
+	diff $dir/tst/$C.1bk $TSTDIR/$C.1
+	exit $?
 fi
-diff $2.1bk $2.1
-exit $?
+exit 0
