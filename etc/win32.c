b1b7783b9821b4afabb029077384e7fb485f0c3e
David Hanson <drh@drhanson.net>
1997-03-07 22:48:55 +0000

    NT client.

--- etc/win32.c
+++ etc/win32.c
@@ -1,4 +1,4 @@
-/* SPARCs running Solaris 2.5.1 at CS Dept., Princeton University */
+/* x86s running MS Windows NT 4.0 */
 
 #include <string.h>
 
@@ -7,44 +7,37 @@ static char rcsid[] = "$Id$";
 #ifndef LCCDIR
 #define LCCDIR "/usr/local/lib/lcc/"
 #endif
-#ifndef SUNDIR
-#define SUNDIR "/opt/SUNWspro/SC4.0/lib/"
+#ifndef VCCDIR
+#define VCCDIR "/program files/devstudio/vc/"
 #endif
 
+char *suffixes[] = { ".c", ".i", ".asm", ".obj", ".exe", 0 };
 char inputs[256] = "";
-char *cpp[] = { LCCDIR "cpp",
-	"-D__STDC__=1", "-Dsparc", "-D__sparc__", "-Dsun", "-D__sun__",
+char *cpp[] = { LCCDIR "cpp.exe",
+	"-D__STDC__=1", "$1", "$2", "$3", 0 };
+char *include[] = { "-I" LCCDIR "include", "\"-I" VCCDIR "include\"", 0 };
+char *com[] = { LCCDIR "rcc.exe", "-target=sparc/solaris",
 	"$1", "$2", "$3", 0 };
-char *include[] = { "-I" LCCDIR "include", "-I/usr/local/include",
-	"-I/usr/include", 0 };
-char *com[] = { LCCDIR "rcc", "-target=sparc/solaris",
-	"$1", "$2", "$3", 0 };
-char *as[] = { "/usr/ccs/bin/as", "-Qy", "-s", "-o", "$3", "$1", "$2", 0 };
-char *ld[] = { "/usr/ccs/bin/ld", "-o", "$3", "$1",
-	SUNDIR "crti.o", SUNDIR "crt1.o",
-	SUNDIR "values-xa.o", "$2", "",
-	"-Y", "P," SUNDIR ":/usr/ccs/lib:/usr/lib", "-Qy",
-	"-L" LCCDIR, "-llcc", "-lm", "-lc", SUNDIR "crtn.o", 0 };
-static char *bbexit = LCCDIR "bbexit.o";
+char *as[] = { "\"/program files/MASM611/bin/ml.exe\"", "-nologo", "-c",
+	"-Fo$3", "$1", "$2", 0 };
+char *ld[] = { "\"" VCCDIR "bin/link.exe\"", "-nologo", 
+	"-align:0x1000", "-subsystem:console", "-entry:mainCRTStartup",
+	"$2", "-OUT:$3", "$1", "", "libc.lib", "kernel32.lib", 0 };
+static char *bbexit = LCCDIR "bbexit.obj";
 
 extern char *concat(char *, char *);
 extern int access(const char *, int);
 
 int option(char *arg) {
 	if (strncmp(arg, "-lccdir=", 8) == 0) {
-		cpp[0] = concat(&arg[8], "/cpp");
-		include[0] = concat("-I", concat(&arg[8], "/include"));
-		ld[12] = concat("-L", &arg[8]);
-		com[0] = concat(&arg[8], "/rcc");
-		bbexit = concat(&arg[8], "/bbexit.o");
+		cpp[0] = concat(&arg[8], "\\cpp.exe");
+		include[0] = concat("-I", concat(&arg[8], "\\include"));
+		com[0] = concat(&arg[8], "\\rcc.exe");
+		bbexit = concat(&arg[8], "\\bbexit.obj");
 	} else if (strcmp(arg, "-g") == 0)
 		;
-	else if (strcmp(arg, "-p") == 0) {
-		ld[5] = SUNDIR "mcrt1.o";
-		ld[10] = "P," SUNDIR "libp:/usr/ccs/lib/libp:/usr/lib/libp:"
-			 SUNDIR ":/usr/ccs/lib:/usr/lib";
-	} else if (strcmp(arg, "-b") == 0 && access(bbexit, 4) == 0)
-		ld[8] = bbexit;
+	else if (strcmp(arg, "-b") == 0 && access(bbexit, 4) == 0)
+		ld[9] = bbexit;
 	else if (strncmp(arg, "-ld=", 4) == 0)
 		ld[0] = &arg[4];
 	else
