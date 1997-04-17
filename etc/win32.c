597d57c3b0bd631fa00901cc1f9d5c00986d8d2d
Chris Fraser <cwf@aya.yale.edu>
1997-04-17 23:45:34 +0000

    Add -Cp to MASM flags, in order to preserve the case of identifiers.

--- etc/win32.c
+++ etc/win32.c
@@ -13,7 +13,7 @@ char inputs[256] = ".;\"/program files/devstudio/vc/include\";/msdev/include";
 char *cpp[] = { LCCDIR "cpp", "-D__STDC__=1", "$1", "$2", "$3", 0 };
 char *include[] = { "-I" LCCDIR "include", 0 };
 char *com[] = { LCCDIR "rcc", "-target=x86/win32", "$1", "$2", "$3", 0 };
-char *as[] = { "ml", "-nologo", "-c", "-Fo$3", "$1", "$2", 0 };
+char *as[] = { "ml", "-nologo", "-c", "-Cp", "-Fo$3", "$1", "$2", 0 };
 char *ld[] = { "link", "-nologo", 
 	"-align:0x1000", "-subsystem:console", "-entry:mainCRTStartup",
 	"$2", "-OUT:$3", "$1", "", "libc.lib", "kernel32.lib", 0 };
