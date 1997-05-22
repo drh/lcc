3d5329bfa4c896aeb0c46d2e961e606602d848d3
David Hanson <drh@drhanson.net>
1997-05-22 22:45:13 +0000

    Cut bbexit, because it's now in liblcc.

--- etc/win32.c
+++ etc/win32.c
@@ -10,14 +10,13 @@ static char rcsid[] = "$Id$";
 
 char *suffixes[] = { ".c;.C", ".i;.I", ".asm;.ASM;.s;.S", ".obj;.OBJ", ".exe", 0 };
 char inputs[256] = ".;\"/program files/devstudio/vc/include\";/msdev/include";
-char *cpp[] = { LCCDIR "cpp", "-D__STDC__=1", "$1", "$2", "$3", 0 };
+char *cpp[] = { LCCDIR "cpp", "-D__STDC__=1", "-Dwin32", "-D_WIN32", "$1", "$2", "$3", 0 };
 char *include[] = { "-I" LCCDIR "include", 0 };
 char *com[] = { LCCDIR "rcc", "-target=x86/win32", "$1", "$2", "$3", 0 };
 char *as[] = { "ml", "-nologo", "-c", "-Cp", "-coff", "-Fo$3", "$1", "$2", 0 };
 char *ld[] = { "link", "-nologo", 
 	"-align:0x1000", "-subsystem:console", "-entry:mainCRTStartup",
-	"$2", "-OUT:$3", "$1", "", "libc.lib", "kernel32.lib", 0 };
-static char *bbexit = LCCDIR "bbexit.obj";
+	"$2", "-OUT:$3", "$1", LCCDIR "liblcc.lib", "libc.lib", "kernel32.lib", 0 };
 
 extern char *concat(char *, char *);
 extern int access(const char *, int);
@@ -31,11 +30,11 @@ int option(char *arg) {
 		cpp[0] = concat(arg, "\\cpp.exe");
 		include[0] = concat("-I", concat(arg, "\\include"));
 		com[0] = concat(arg, "\\rcc.exe");
-		bbexit = concat(arg, "\\bbexit.obj");
+		ld[8] = concat(arg, "\\liblcc.lib");
 	} else if (strcmp(arg, "-g") == 0)
+		ld[9] = "libcd.lib";
+	else if (strcmp(arg, "-b") == 0)
 		;
-	else if (strcmp(arg, "-b") == 0 && access(bbexit, 4) == 0)
-		ld[9] = bbexit;
 	else if (strncmp(arg, "-ld=", 4) == 0)
 		ld[0] = &arg[4];
 	else
