3a8a233233c14324b413c73eb3a6dbe4275fce32
David Hanson <drh@drhanson.net>
1997-05-22 22:46:51 +0000

    Revised diagnostic to show file, line number.

--- lib/yynull.c
+++ lib/yynull.c
@@ -6,7 +6,7 @@ static char rcsid[] = "$Id$";
 void _YYnull(char *file, int line) {
 	fprintf(stderr, "null pointer dereferenced:");
 	if (file)
-		fprintf(stderr, " file %s", file);
+		fprintf(stderr, " file %s,", file);
 	fprintf(stderr, " line %d\n", line);
 	fflush(stderr);
 	abort();
