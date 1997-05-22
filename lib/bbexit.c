7154c7359330d4eb12994224a8b042272ebe31df
David Hanson <drh@drhanson.net>
1997-05-22 22:46:11 +0000

    Addd $Id$. Cut exit(), added atexit call. Now determines endianness on the fly.

--- lib/bbexit.c
+++ lib/bbexit.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+static char rcsid[] = "$Id$";
 
 struct callsite {
 	char *file, *name;
 	union coordinate {
-		struct {
-#if MIPSEB || sparc
-			unsigned int index:6,x:10,y:16;
-#else
-			unsigned int y:16,x:10,index:6;
-#endif
-		} c;
+		struct { unsigned int index:6,x:10,y:16; } be;
+		struct { unsigned int y:16,x:10,index:6; } le;
 		unsigned int coord;
 	} u;
 } *_caller;
@@ -31,36 +29,24 @@ static struct _bbdata {
 	} *funcs;
 } tail, *_bblist = &tail;
 
-void _epilogue(struct func *callee) {
-	_caller = 0;
-}
+static void unpack(unsigned int coord, int *index, int *x, int *y) {
+	static union { int x; char endian; } little = { 1 };
+	union coordinate u;
 
-void _prologue(struct func *callee, struct _bbdata *yylink) {
-	static struct caller callers[4096];
-	static int next;
-	struct caller *p;
-
-	if (!yylink->link) {
-		yylink->link = _bblist;
-		_bblist = yylink;
+	u.coord = coord;
+	if (little.endian) {
+		*index = u.le.index;
+		*x = u.le.x;
+		*y = u.le.y;
+	} else {
+		*index = u.be.index;
+		*x = u.be.x;
+		*y = u.be.y;
 	}
-	for (p = callee->callers; p; p = p->link)
-		if (p->caller == _caller) {
-			p->count++;
-			break;
-		}
-	if (!p && next < sizeof callers/sizeof callers[0]) {
-		p = &callers[next++];
-		p->caller = _caller;
-		p->count = 1;
-		p->link = callee->callers;
-		callee->callers = p;
-	}
-	_caller = 0;
-}
+}				
 
 static void profout(struct _bbdata *p, FILE *fp) {
-	int i;
+	int i, index, x, y;
 	struct func *f;
 	struct caller *q;
 
@@ -77,25 +63,27 @@ static void profout(struct _bbdata *p, FILE *fp) {
 	for (f = p->funcs; f; f = f->link) {
 		int n = 0;
 		for (q = f->callers; q; n += q->count, q = q->link) {
-			fprintf(fp, "%s %d %d %d %d", f->name, f->src.c.index,
-				f->src.c.x, f->src.c.y, q->count);
-			if (q->caller)
-				fprintf(fp, " %s %s %d %d\n", q->caller->name, q->caller->file,
-					q->caller->u.c.x, q->caller->u.c.y);
-			else
+			unpack(f->src.coord, &index, &x, &y);
+			fprintf(fp, "%s %d %d %d %d", f->name, index, x, y, q->count);
+			if (q->caller) {
+				unpack(q->caller->u.coord, &index, &x, &y);
+				fprintf(fp, " %s %s %d %d\n", q->caller->name, q->caller->file, x, y);
+			} else
 				fprintf(fp, " ? ? 0 0\n");
 		}
-		if (n == 0)
-			fprintf(fp, "%s %d %d %d 0 ? ? 0 0\n", f->name, f->src.c.index,
-				f->src.c.x, f->src.c.y);
+		if (n == 0) {
+			unpack(f->src.coord, &index, &x, &y);
+			fprintf(fp, "%s %d %d %d 0 ? ? 0 0\n", f->name, index, x, y);
+		}
 	}		
 	fprintf(fp, "%d\n", p->npoints);
-	for (i = 0; i < p->npoints; i++)
-		fprintf(fp, "%d %d %d %d\n", p->coords[i].c.index, p->coords[i].c.x,
-			p->coords[i].c.y, p->counts[i]);
+	for (i = 0; i < p->npoints; i++) {
+		unpack(p->coords[i].coord, &index, &x, &y);
+		fprintf(fp, "%d %d %d %d\n", index, x, y, p->counts[i]);
+	}
 }
 
-void exit(int code) {
+static void bbexit(void) {
 	FILE *fp;
 
 	if (_bblist != &tail && (fp = fopen("prof.out", "a"))) {
@@ -103,8 +91,34 @@ void exit(int code) {
 			profout(_bblist, fp);
 		fclose(fp);
 	}
-#if mips || sparc
-	{ extern void _cleanup(void); _cleanup(); }
-#endif
-	{ extern void _exit(int); _exit(code); }
+}
+
+void _epilogue(struct func *callee) {
+	_caller = 0;
+}
+
+void _prologue(struct func *callee, struct _bbdata *yylink) {
+	static struct caller callers[4096];
+	static int next;
+	struct caller *p;
+
+	if (!yylink->link) {
+		yylink->link = _bblist;
+		_bblist = yylink;
+		if (next == 0)
+			atexit(bbexit);
+	}
+	for (p = callee->callers; p; p = p->link)
+		if (p->caller == _caller) {
+			p->count++;
+			break;
+		}
+	if (!p && next < sizeof callers/sizeof callers[0]) {
+		p = &callers[next++];
+		p->caller = _caller;
+		p->count = 1;
+		p->link = callee->callers;
+		callee->callers = p;
+	}
+	_caller = 0;
 }
