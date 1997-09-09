42fa58fd48c2010bb0de8ab9a38e40b76a2a0495
David Hanson <drh@drhanson.net>
1997-09-09 22:57:53 +0000

    Added -lm, rearranged ld's arguments.

--- etc/linux.c
+++ etc/linux.c
@@ -16,16 +16,17 @@ char *cpp[] = { LCCDIR "cpp",
 	"-D__unix__", "-D__i386__", "-D__linux__",
 	"$1", "$2", "$3", 0 };
 char *include[] = {"-I" LCCDIR "include", "-I/usr/include", 0 };
-char *com[] = {LCCDIR "rcc", "-target=x86/linux", $1", "$2", "$3", 0 };
+char *com[] = {LCCDIR "rcc", "-target=x86/linux", "$1", "$2", "$3", 0 };
 char *as[] = { "/usr/bin/as", "-o", "$3", "$1", "$2", 0 };
 char *ld[] = {
 	/*  0 */ "/usr/bin/ld", "-m", "elf_i386", "-dynamic-linker",
 	/*  4 */ "/lib/ld-linux.so.1", "-o", "$3",
 	/*  7 */ "/usr/lib/crt1.o", "/usr/lib/crti.o", "/usr/lib/crtbegin.o", 
-                 "$1", "$2", "-lc",
-	/* 13 */ "-L" LCCDIR,
-	/* 14 */ "-llcc",
-	/* 15 */ "", "/usr/lib/crtend.o", "/usr/lib/crtn.o",
+                 "$1", "$2",
+	/* 12 */ "-L" LCCDIR,
+	/* 13 */ "-llcc",
+	/* 14 */ "", "", "-lc", "-lm",
+	/* 18 */ "", "/usr/lib/crtend.o", "/usr/lib/crtn.o",
 	0 };
 
 extern char *concat(char *, char *);
@@ -34,11 +35,11 @@ int option(char *arg) {
   	if (strncmp(arg, "-lccdir=", 8) == 0) {
 		cpp[0] = concat(&arg[8], "/cpp");
 		include[0] = concat("-I", concat(&arg[8], "/include"));
-		ld[13] =  concat("-L", &arg[8]);
+		ld[12] =  concat("-L", &arg[8]);
 		com[0] = concat(&arg[8], "/rcc");
 	} else if (strcmp(arg, "-p") == 0 || strcmp(arg, "-pg") == 0) {
 		ld[7] = "/usr/lib/gcrt1.o";
-		ld[15] = "-lgmon";
+		ld[18] = "-lgmon";
 	} else if (strcmp(arg, "-b") == 0) 
 		;
 	else if (strcmp(arg, "-g") == 0)
@@ -48,8 +49,8 @@ int option(char *arg) {
 	else if (strcmp(arg, "-static") == 0) {
 	        ld[3] = "-static";
 	        ld[4] = "";
-	        ld[13] = "-L/usr/lib/gcc-lib/i486-linux/2.7.2";
-	        ld[14] = "-lgcc";
+	        ld[14] = "-L/usr/lib/gcc-lib/i486-linux/2.7.2";
+	        ld[15] = "-lgcc";
 	} else
 		return 0;
 	return 1;
