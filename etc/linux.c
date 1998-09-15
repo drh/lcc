c58df8f4c78828629304435c091c9e8a8861e0f2
David Hanson <drh@drhanson.net>
1998-09-15 22:52:23 +0000

    Revised to use gcc's C preprocessor and headers.

--- etc/linux.c
+++ etc/linux.c
@@ -10,12 +10,12 @@ static char rcsid[] = "$Id$";
 
 char *suffixes[] = { ".c", ".i", ".s", ".o", ".out", 0 };
 char inputs[256] = "";
-char *cpp[] = { LCCDIR "cpp",
+char *cpp[] = { LCCDIR "gcc/cpp",
 	"-U__GNUC__", "-D_POSIX_SOURCE", "-D__STDC__=1", "-D__STRICT_ANSI__",
 	"-Dunix", "-Di386", "-Dlinux",
-	"-D__unix__", "-D__i386__", "-D__linux__",
+	"-D__unix__", "-D__i386__", "-D__linux__", "-D__signed__=signed",
 	"$1", "$2", "$3", 0 };
-char *include[] = {"-I" LCCDIR "include", "-I/usr/include", 0 };
+char *include[] = {"-I" LCCDIR "include", "-I" LCCDIR "gcc/include", "-I/usr/include", 0 };
 char *com[] = {LCCDIR "rcc", "-target=x86/linux", "$1", "$2", "$3", 0 };
 char *as[] = { "/usr/bin/as", "-o", "$3", "$1", "$2", 0 };
 char *ld[] = {
@@ -33,8 +33,9 @@ extern char *concat(char *, char *);
 
 int option(char *arg) {
   	if (strncmp(arg, "-lccdir=", 8) == 0) {
-		cpp[0] = concat(&arg[8], "/cpp");
+		cpp[0] = concat(&arg[8], "/gcc/cpp");
 		include[0] = concat("-I", concat(&arg[8], "/include"));
+		include[1] = concat("-I", concat(&arg[8], "/gcc/include"));
 		ld[12] =  concat("-L", &arg[8]);
 		com[0] = concat(&arg[8], "/rcc");
 	} else if (strcmp(arg, "-p") == 0 || strcmp(arg, "-pg") == 0) {
