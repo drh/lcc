c7bcd7730bc75336302d585fa9da8552a005de51
David R. Hanson <drh@drhanson.net>
2021-01-11 15:23:21 -0700

    Remove FAQ and Extra pages. Remove unused directories and files. Use htmlpreview.github.io to display install.html.

--- 4.2/solaris.mk
+++
@@ -1,8 +0,0 @@
-BUILDDIR=sparc-solaris
-TARGET=sparc/solaris
-TSTDIR=$(TARGET)/tst
-HOSTFILE=etc/solaris.c
-CFLAGS=-g -DSUNDIR=\"/usr/local/opt/SUNWspro/SC5.0/lib/\"
-LDFLAGS=-g
-CC=/usr/local/opt/SUNWspro/bin/cc
-LD=/usr/local/opt/SUNWspro/bin/cc
